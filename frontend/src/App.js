23924dee7e42bc6f55673064160ff804f6432f55
adityatm28 <adityatm28@gmail.com>
2022-02-12 01:47:25 +0530

    product-data-design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,33 @@
+import Data from './data';
+
 function App() {
   return (
     <div className="App">
       <header>
         <a href="/">Amazona</a>
       </header>
-      <main>List Product</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {Data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <p>
+                  {' '}
+                  <a href={`/product/${product.slug}`}>{product.name}</a>
+                </p>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button type="button">Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
